10807440d6b8d9914faf8443c815ba2ac046f238
KCI <vickyagassi996@gmail.com>
2025-08-19 01:35:44 +0700

    blade component

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $data=["title" => "Home Page"];
+    return view('home', $data);
+});
+
+Route::get('/about', function () {
+    $data = ["title" => "About Page","name" => "Mohammad Vicky Agassi"];
+    return view('about', $data);
+});
+
+Route::get('/blog', function () {
+    $data = ["title" => "Blog Page"];
+    return view('blog', $data);
+});
+
+Route::get('/contact', function () {
+    $data = [
+        "title" => "Contact Page",
+        "name" => "Mohammad Vicky Agassi",
+        "email" => "z0a9u@example.com",
+        "phone" => "08123456789",
+        "social_media" => "https://github.com/MohammadVickyAgassi"
+    ];
+    return view('contact', $data);
 });
