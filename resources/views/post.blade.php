00065f0393e610f2ac4ce2fd5a573a4a47de6bde
KCI <vickyagassi996@gmail.com>
2025-08-19 10:23:03 +0700

    model factories

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
         </h2>
 
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post["author"] }}</a> | {{ $post["created_at"] }}
+            <a href="#">{{ $post["author"] }}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="my-4 font-light">
             {{ $post["body"] }}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
             </a>
 
             <div class="text-base text-gray-500">
-                <a href="#">{{ $post["author"] }}</a> | {{ $post["created_at"] }}
+                <a href="#">{{ $post["author"] }}</a> | {{ $post->created_at->diffForHumans() }}
             </div>
             <p class="my-4 font-light">
                 {{ Str::limit($post["body"], 200) }}
